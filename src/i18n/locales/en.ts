74d9575b8a2da482b9801e8f3a9533020f07e2d0
kai <kiki_wang1021@163.com>
2025-04-17 17:33:07 +0800

    fix：移除冗余文件

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,12 +1,6 @@
 export default {
   nav: {
     home: 'Home',
-    industry: 'Industry Solutions',
-    products: 'Products',
-    customer: 'Case Studies',
-    partners: 'Partners',
-    about: 'About Us',
-    news: 'News'
   },
   common: {
     phone: '400-8375-7867',
--- src/i18n/locales/zh.ts
+++ src/i18n/locales/zh.ts
@@ -1,12 +1,6 @@
 export default {
   nav: {
     home: '首页',
-    industry: '行业方案',
-    products: '产品中心',
-    customer: '客户案例',
-    partners: '合作伙伴',
-    about: '关于我们',
-    news: '新闻资讯'
   },
   common: {
     phone: '400-8375-7867',
